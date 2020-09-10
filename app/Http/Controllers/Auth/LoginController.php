43f05b135bbb5ece1964c8cd8b035ab3f52e1898
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2020-09-10 21:38:30 +0600

    redirect url changed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,11 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function redirectTo()
+    {
+        return Auth::user()->role->slug === 'shop-owner' ? '/app/orders/process/' : '/app/dashboard';
+    }
 
     /**
      * Create a new controller instance.
