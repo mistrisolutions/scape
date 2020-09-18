dd24ca873bf6e3821381ab352b920356565db359
shuvo <shuvodewan.sky@gmail.com>
2020-09-18 06:13:59 +0600

    order view composer update

--- app/Http/View/Composers/OrderComposer.php
+++ app/Http/View/Composers/OrderComposer.php
@@ -14,7 +14,7 @@ class OrderComposer
         $view->with('owners',ShopOwner::all());
         $view->with('methods',PaymentMethod::all());
       //   /dd($view->getName());
-        if(!in_array($view->getName(), ['forntend.confirmation', 'forntendorder', 'fornend.tracking', 'auth.login','layouts.frontend.app','layouts.frontend.partials.header','backend.notifications.index'])) {
+        if(!in_array($view->getName(), ['forntend.confirmation', 'forntend.order', 'forntend.tracking', 'auth.login','layouts.frontend.app','layouts.frontend.partials.header','backend.notifications.index'])) {
          //dd($view->getName());
          $view->with('notifications',Notification::checkAuth()->latest('id')->limit(5)->get());
      }
